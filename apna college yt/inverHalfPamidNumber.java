bc793938ae4b013fd7627fe489f295c9b4b31b8b
Vikas sharma <vikassharma85109@gmail.com>
2025-10-08 14:09:24 +0530

    init

--- apna college yt/inverHalfPamidNumber.java
+++ apna college yt/inverHalfPamidNumber.java
@@ -1,3 +1,14 @@
 public class inverHalfPamidNumber {
-  
+  public static void main(String[] args) {
+    int n = 5;
+
+    //loop for row
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <=n-i+1; j++) {
+        System.out.print(j+" ");
+        
+      }
+      System.out.println();
+     }
+  }
 }
