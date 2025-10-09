c26a396e2fd4a666a7aed1879899a52b0b8099e5
Vikas sharma <vikassharma85109@gmail.com>
2025-10-09 13:25:47 +0530

    function lect

---
+++ apna college yt/functionAndMethod/namePrintFunction.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class namePrintFunction {
+
+  public static void printMyName(String name) {
+    System.out.println("Your name "+name);
+    return;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter Your Name ");
+    String name = sc.nextLine();
+
+
+    printMyName(name);
+
+  }
+  
+}
---
+++ apna college yt/functionAndMethod/sumTwoNum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class sumTwoNum {
+
+  public static int calculateSum(int a, int b) {
+   int sum = a + b;
+    return sum;
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter two number");
+
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+
+    int sum = calculateSum(a, b);
+    System.out.println("Sum of two number "+ sum);
+
+  }
+}
