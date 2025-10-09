73bd7566b04b120f028cc2526aede420f70045a2
Vikas sharma <vikassharma85109@gmail.com>
2025-10-09 16:46:20 +0530

    practice

--- apna college yt/functionAndMethod/practiceFunctionQuestion.java
+++ apna college yt/functionAndMethod/practiceFunctionQuestion.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class practiceFunctionQuestion {
+  public static int calculateAverage(int a, int b, int c) {
+    return (a + b + c) / 3;
+  }
   public static void main(String[] args) {
-    System.out.println("Hello");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter 3 number ");
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+
+    int ans = calculateAverage(a, b, c);
+    System.out.println(ans);
   }
 }
