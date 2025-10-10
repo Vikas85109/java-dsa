056018134155b3364039eee267666c4073f97f9e
Vikas sharma <vikassharma85109@gmail.com>
2025-10-10 12:40:47 +0530

    infiniteloop

---
+++ apna college yt/practiceQuestionfun/question06.java
@@ -0,0 +1,11 @@
+public class question06 {
+  public static void main(String[] args) {
+    // Write an infinite loop using do while condition.
+    
+    int i = 1;
+    while (true) {
+      System.out.println(i);
+      i++;
+    }
+  }
+}
